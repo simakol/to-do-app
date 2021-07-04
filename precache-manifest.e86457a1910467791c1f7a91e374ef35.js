ad92bb91183b0645f4c66492ab251f7a7f9c5e6b
Sempank <wwwoleksiy12345@gmail.com>
2021-07-04 13:47:32 +0300

    Updates

---
+++ precache-manifest.e86457a1910467791c1f7a91e374ef35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6de00efa1fa0c478f61ab2e6106bcc",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "a124161bf5072bebaa12",
+    "url": "/to-do-app/static/css/2.023c26ae.chunk.css"
+  },
+  {
+    "revision": "3287e6c5d904b74a1b87",
+    "url": "/to-do-app/static/css/main.e05a26f2.chunk.css"
+  },
+  {
+    "revision": "a124161bf5072bebaa12",
+    "url": "/to-do-app/static/js/2.331a3ce3.chunk.js"
+  },
+  {
+    "revision": "3287e6c5d904b74a1b87",
+    "url": "/to-do-app/static/js/main.75a61438.chunk.js"
+  },
+  {
+    "revision": "6cf4045563d5e41dd4b3",
+    "url": "/to-do-app/static/js/runtime~main.01361c46.js"
+  },
+  {
+    "revision": "b9da2244b48e97577590dd8137200f51",
+    "url": "/to-do-app/static/media/background.b9da2244.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/to-do-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/to-do-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/to-do-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/to-do-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/to-do-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
